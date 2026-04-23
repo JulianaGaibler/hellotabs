3b8c39e9ee3b58d03d94ce6547dd764e0610eb37
Juliana Gaibler <git@juliana.me>
2026-04-23 03:45:54 +0200

    🐛 Fixed CI

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -8,8 +8,18 @@ declare module '*?raw' {
   export default value
 }
 
+declare module '*.sass' {
+  const value: string
+  export default value
+}
+
 declare const __VERSION__: string
 declare const __FIREFOX__: boolean
+
+interface Window {
+  chrome?: typeof chrome
+  browser?: typeof browser
+}
 
 declare module 'virtual:available-locales' {
   export const availableLocales: string[]
